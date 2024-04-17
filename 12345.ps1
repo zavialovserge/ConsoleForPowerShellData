f75a224130132fe3c58b03eb113ccec9b10db12f
SOFTSERVE\szavia <zavialovserge@gmail.com>
2024-04-17 19:22:49 +0300

    Add powershell script

---
+++ 12345.ps1
@@ -0,0 +1,21 @@
+﻿$sqlConn = New-Object System.Data.SqlClient.SqlConnection
+$sqlConn.ConnectionString = “Data Source=CVWCYD3\SQLEXPRESS;Initial Catalog=ProductTest;Integrated Security=True;”
+$sqlConn.Open()
+$sqlcmd = $sqlConn.CreateCommand()
+$sqlcmd.Connection = $sqlConn
+$query = “EXEC GetProduct”
+$sqlcmd.CommandText = $query
+$reader = $sqlcmd.ExecuteReader()
+$data = 
+while($reader.Read())
+{
+   
+     [PSCustomObject] @{
+        ID = $reader["ID"]
+        Name = $reader["Name"]
+       Description= $reader["Description"]
+  
+  }
+}
+$sqlConn.Close()
+return $data
